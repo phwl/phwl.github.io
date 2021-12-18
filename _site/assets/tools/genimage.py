df06fa4fe12d8f43e76ecf7a48932e5fdc547be5
Philip Leong <philip.leong@sydney.edu.au>
2021-12-18 20:45:15 +1100

    add .gitignore

--- _site/assets/tools/genimage.py
+++
@@ -1,7 +0,0 @@
-import fileinput
-import re
- 
-for line in fileinput.input(inplace=1):
-    #line = re.sub('wordpress_id: \d+','classes: wide', line.rstrip())
-    line = re.sub('^(.*)$',r'{% include figure image_path="\1" caption="" %}', line.rstrip())
-    print(line)
--- _site/assets/tools/r.py
+++
@@ -1,7 +0,0 @@
-import fileinput
-import re
- 
-for line in fileinput.input(inplace=1, backup='.bak'):
-    #line = re.sub('wordpress_id: \d+','classes: wide', line.rstrip())
-    line = re.sub('\[!\[(.*)\]\((.*)\)\{: .align-center\}\]\(/.*$',r'{% include figure image_path="\2" caption="\1" %}', line.rstrip())
-    print(line)
